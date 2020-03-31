02701f4d7c74c696761b6acc404504c7a7cbd8b8
SergeyArenzon <sergeyr1991@gmail.com>
2020-03-31 18:25:33 +0300

    twitter api example

--- app.js
+++ app.js
@@ -1,32 +1,115 @@
 
-var Twitter = require('twitter');
-var config = require('./config.js');
-var T = new Twitter(config);
+// var Twitter = require('twitter');
+// var config = require('./config.js');
+// var T = new Twitter(config);
 
 
-// Set up your search parameters
-var params = {
-  q: 'trump',
-  count: 1,
-  result_type: 'recent',
-  lang: 'en',
+// // Set up your search parameters
+// var params = {
+//   q: 'trump',
+//   count: 100,
+//   result_type: 'recent',
+//   lang: 'en',
   
-}
-T.get('search/tweets', params, function(err, data, response) {
-  if(!err){
-    // Loop through the returned tweets
-    for(let i = 0; i < data.statuses.length; i++){
-      // Get the tweet Id from the returned data
-      //let id = { id: data.statuses[i].id_str }
+  
+  
+// }
+// T.get('search/tweets', params, function(err, data, response) {
+//   if(!err){
+//     // Loop through the returned tweets
+//     for(let i = 0; i < data.statuses.length; i++){
+//       // Get the tweet Id from the returned data
+//       //let id = { id: data.statuses[i].id_str }
 
-      console.log(data.statuses[i]);
-      console.log("****");
-      console.log(data.statuses[i].text);
-    }
+//       //console.log(data.statuses[i]);
+//       console.log("****");
+//       console.log(data.statuses[i].text);
+//     }
     
-  } 
+//   } 
   
-  else {
-    console.log(err);
+//   else {
+//     console.log(err);
+//   }
+// })
+
+
+
+
+const https = require('https');
+const request = require('request');
+const util = require('util');
+
+
+
+
+
+const get = util.promisify(request.get);
+const post = util.promisify(request.post);
+
+const consumer_key = 'fgFMaMMgnSfd63VoXxYrHdnGl';                             // Add your API key here
+const consumer_secret = '1ctcEkSoT7jLRVba3xKeufTKGPRIJzACS8tLuhPUYzWibI1gtG'; // Add your API secret key here
+
+const bearerTokenURL = new URL('https://api.twitter.com/oauth2/token');
+const searchURL = new URL('https://api.twitter.com/labs/2/tweets/search');
+
+async function bearerToken (auth) {
+  const requestConfig = {
+    url: bearerTokenURL,
+    auth: {
+      user: consumer_key,
+      pass: consumer_secret,
+    },
+    form: {
+      grant_type: 'client_credentials',
+    },
+  };
+
+  const response = await post(requestConfig);
+  return JSON.parse(response.body).access_token;
+}
+
+(async () => {
+  let token;
+  const query = 'netanyahu';
+  const maxResults = 100;
+
+  try {
+    // Exchange your credentials for a Bearer token
+    token = await bearerToken({consumer_key, consumer_secret});
+  } catch (e) {
+    console.error(`Could not generate a Bearer token. Please check that your credentials are correct and that the Filtered Stream preview is enabled in your Labs dashboard. (${e})`);
+    process.exit(-1);
   }
-})
+
+  const requestConfig = {
+    url: searchURL,
+    qs: {
+      query: query,
+      max_results: maxResults,
+      
+    },
+    auth: {
+      bearer: token,
+    },
+    headers: {
+      'User-Agent': 'LabsRecentSearchQuickStartJS',
+    },
+    json: true,
+  };
+
+  try {
+    const res = await get(requestConfig);  
+    //console.log(res.statusCode);
+    console.log(res.body.data);
+    if (res.statusCode !== 200) {
+      throw new Error(res.json);
+      return;
+    }
+
+    //console.log(res.json);
+  } catch (e) {
+    console.error(`Could not get search results. An error occurred: ${e}`);
+    process.exit(-1);
+  }
+})();
--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    consumer_key: 'tWWuN3ATNKIdGgklVGqYn67nX',
-    consumer_secret: 'vznx0iD1Tkq5WRT5CU6kOzyyOAaUOsTmamWROwPNHJC54ualEL',
-    access_token_key: '1240329397468618768-yFptfk14phXdke2sXpXBIMdDx1ZwZ5',
-    access_token_secret: 'jp4l00yEFLVHmzaaWnYCHGfrZKqi6euWNX6yoYFqj5sQG'
+    consumer_key: 'fgFMaMMgnSfd63VoXxYrHdnGl',
+    consumer_secret: '1ctcEkSoT7jLRVba3xKeufTKGPRIJzACS8tLuhPUYzWibI1gtG',
+    access_token_key: '1240329397468618768-8nS8yWZsnJDObgxWH7uLFp6P9rqPzg',
+    access_token_secret: 'VUWkn8gCYDmlE5ggkKHVXFi8H1un4VPNwwBECfjbdwKvp'
 }
